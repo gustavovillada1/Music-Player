8800ac4e1a5552a6c852186e8e2440aaa8a9a3a3
Gustavo Adolfo Villada Molina <gustavo-g_10@hotmail.com>
2025-05-06 19:35:22 -0500

    Initial Commit

---
+++ SpotifyClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotifyClone
+//
+//  Created by Gustavo Adolfo Villada Molina on 6/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Gustavo Adolfo Villada Molina on 6/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
